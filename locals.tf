63e40f0ef1d4a9c2c108d7c7308573220ea1724f
Zachary Choate <zach.a.choate@gmail.com>
2023-06-17 15:45:56 -0400

    add short prefix

--- locals.tf
+++ locals.tf
@@ -7,8 +7,10 @@ resource "random_string" "rand" {
 }
 
 locals {
-    resource_prefix_unclean = "${var.location_short}-${var.project_short}-${var.environment}-${random_string.rand.result}"
-    resource_prefix = lower(local.resource_prefix_unclean)
+    resource_prefix_partial = "${var.location_short}-${var.project_short}-${var.environment}-${random_string.rand.result}"
+    resource_prefix = lower(local.resource_prefix_partial)
+    resource_prefix_short_partial = substr(replace(lower("${var.location_short}${var.project_short}"), "/\\W/", ""),0, 14)
+    resource_prefix_short = "${local.resource_prefix_short_partial}${random_string.rand.result}"
     tags = tomap({
         "project"   = var.project
     })
